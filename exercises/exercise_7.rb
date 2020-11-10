8e5f968f16cef1eff826adf79455f4778897ba28
Etienne LC <etlecoent@gmail.com>
2020-11-10 00:18:57 +0000

    Completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print ">"
+storeX_name = gets.chomp
+@storeX = Store.create(name: storeX_name)
+
+@storeX.errors.each do |attribute, error|
+  puts "#{attribute} #{error}"
+end
+
+print ">"
+storeY_name = gets.chomp
+@storeY = Store.create(name: storeY_name, annual_revenue: 1, mens_apparel: false, womens_apparel: false)
+
+@storeY.errors.each do |attribute, error|
+  puts "#{attribute} #{error}"
+end
